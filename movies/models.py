b287ab0e5393e038a6929a0d6800eb27ee91310c
dana sheer <danasheer21111@gmail.com>
2023-02-06 20:41:04 +0300

    first

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Movies_Model(models.Model):
+    name = models.CharField(max_length=150)
+    actors = models.CharField(max_length=100)
+    release_date = models.DateField()
+    
+class Watchlist_Model(models.Model):
+    name = models.CharField(max_length=30)
+
+class Genre_Model(models.Model):
+    name = models.CharField(max_length=30)
+
+class Rating_Reviews(models.Model):
+    reviws_field = models.TextField()
+    
+
+
